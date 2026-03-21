aa3df4551a32c9fa3598c4c3e17610de365fba42
Morty <zaharzvagin254@gmail.com>
2026-03-22 01:45:59 +0300

    Create AdAstraCreate.java

---
+++ src/main/java/com/mortylovelly/adastracreate/AdAstraCreate.java
@@ -0,0 +1,11 @@
+package com.mortylovelly.adastracreate;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AdAstraCreate.MOD_ID)
+public class AdAstraCreate {
+    public static final String MOD_ID = "adastra_create";
+
+    public AdAstraCreate() {
+    }
+}
