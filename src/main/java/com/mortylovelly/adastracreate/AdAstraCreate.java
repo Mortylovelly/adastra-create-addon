341362cca488df96451653f8f4ec4f92713c8aa7
Morty <zaharzvagin254@gmail.com>
2026-03-22 18:37:42 +0300

    Update AdAstraCreate.java

--- src/main/java/com/mortylovelly/adastracreate/AdAstraCreate.java
+++ src/main/java/com/mortylovelly/adastracreate/AdAstraCreate.java
@@ -1,11 +1,15 @@
 package com.mortylovelly.adastracreate;
 
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(AdAstraCreate.MOD_ID)
 public class AdAstraCreate {
     public static final String MOD_ID = "adastra_create";
 
     public AdAstraCreate() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModItems.ITEMS.register(bus);
     }
 }
