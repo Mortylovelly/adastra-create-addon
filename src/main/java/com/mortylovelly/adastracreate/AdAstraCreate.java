bfc76be68b0a0f699a42d15b82c92dc3afc8b792
Morty <zaharzvagin254@gmail.com>
2026-03-22 22:29:55 +0300

    Update AdAstraCreate.java

--- src/main/java/com/mortylovelly/adastracreate/AdAstraCreate.java
+++ src/main/java/com/mortylovelly/adastracreate/AdAstraCreate.java
@@ -1,8 +1,11 @@
 package com.mortylovelly.adastracreate;
 
+import com.mortylovelly.adastracreate.client.JupiterRenderer;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 
 @Mod(AdAstraCreate.MOD_ID)
 public class AdAstraCreate {
@@ -12,5 +15,10 @@ public class AdAstraCreate {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         ModItems.ITEMS.register(bus);
         ModItems.TABS.register(bus);
+
+        // Регистрируем рендерер Юпитера только на клиенте
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            JupiterRenderer.register();
+        }
     }
 }
