6914a2a4ff5e001935762c1ff4fd2a1ed7ff5c2e
Morty <zaharzvagin254@gmail.com>
2026-03-22 20:49:37 +0300

    Update ModItems.java

--- src/main/java/com/mortylovelly/adastracreate/ModItems.java
+++ src/main/java/com/mortylovelly/adastracreate/ModItems.java
@@ -1,5 +1,8 @@
 package com.mortylovelly.adastracreate;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -9,6 +12,18 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
         DeferredRegister.create(ForgeRegistries.ITEMS, AdAstraCreate.MOD_ID);
 
+    public static final DeferredRegister<CreativeModeTab> TABS =
+        DeferredRegister.create(Registries.CREATIVE_MODE_TAB, AdAstraCreate.MOD_ID);
+
     public static final RegistryObject<Item> SUPERALLOY_PLATE =
         ITEMS.register("superalloy_plate", () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<CreativeModeTab> MOD_TAB =
+        TABS.register("adastra_create_tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.adastra_create"))
+            .icon(() -> SUPERALLOY_PLATE.get().getDefaultInstance())
+            .displayItems((params, output) -> {
+                output.accept(SUPERALLOY_PLATE.get());
+            })
+            .build());
 }
