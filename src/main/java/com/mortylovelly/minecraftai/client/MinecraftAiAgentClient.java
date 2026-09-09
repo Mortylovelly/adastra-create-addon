aa84bd30e0344ab21d8c3ca8de1830a82a6ac379
Morty <zaharzvagin254@gmail.com>
2026-09-09 20:50:27 +0300

    Add in-game AI panel keybind

---
+++ src/main/java/com/mortylovelly/minecraftai/client/MinecraftAiAgentClient.java
@@ -0,0 +1,28 @@
+package com.mortylovelly.minecraftai.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public final class MinecraftAiAgentClient implements ClientModInitializer {
+    private static KeyBinding openPanelKey;
+
+    @Override
+    public void onInitializeClient() {
+        openPanelKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.minecraft_ai_agent.open_panel",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_GRAVE_ACCENT,
+                "category.minecraft_ai_agent"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openPanelKey.wasPressed()) {
+                client.setScreen(new AiChatScreen());
+            }
+        });
+    }
+}
